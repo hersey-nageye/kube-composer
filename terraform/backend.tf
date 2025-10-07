64b4e06bd4ff95d936c2c7b1f83bfbc7f8cc2c79
hersey-nageye <hersey_nageye1@hotmail.com>
2025-10-07 14:53:39 +0100

    editing pipeline to fix errors

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,11 @@
 # Remote backend configuration for Terraform state management.
 terraform {
   backend "s3" {
-    bucket       = "eks-project-tfstate"
-    key          = "eks/dev/terraform.tfstate"
-    region       = "eu-west-2"
-    use_lockfile = true
+    bucket            = "eks-project-tfstate"
+    key               = "eks/dev/terraform.tfstate"
+    region            = "eu-west-2"
+    dynamodb_endpoint = "eks-projects-locks"
+    encrypt           = true
   }
 
 }
